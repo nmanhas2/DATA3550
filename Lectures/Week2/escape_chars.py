a011c3acaafe38038bcbb8fcf021a7ab57c477e8
nmanhas2 <manhasnubal@gmail.com>
2025-01-16 19:58:47 -0700

    added methods and indexing practice to lecture folder

---
+++ Lectures/Week2/escape_chars.py
@@ -0,0 +1,15 @@
+#Need to use single quotes if we
+#want to include double quotes in
+#our output
+print('He said "Hello!"')
+
+#Same for the opposite
+print("He said 'Hello!'")
+
+#You can do it this way too
+print("He said \"Hello!\" in quotes")
+
+#Newline 
+print("John Smith\nis the best!!")
+print("\nFleas\n\nAdam\nHad `em.\n\n(Anon.)")
+
---
+++ Lectures/Week2/methods_indexing.py
@@ -0,0 +1,16 @@
+event = "Snow Showers"
+print(event[5].lower())
+print(event[-2].upper())
+
+print(len(event))
+
+#slicing
+print(event[9:12]) #exclusive
+
+#concatenate
+firstName = "Harry"
+lastName = "Potter"
+print(firstName + " " +lastName)
+
+#string alignment functions
+print("\nHAPPY".ljust(20), "BIRTHDAY")
