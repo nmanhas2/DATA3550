688094f73205a53696bd3ef632b00be4cfbd3073
nmanhas2 <manhasnubal@gmail.com>
2025-01-18 17:36:08 -0700

    added greater problem

---
+++ Lectures/Week3/greater.py
@@ -0,0 +1,7 @@
+numOne = int(input("Enter a number: "))
+numTwo = int(input("Enter a 2nd number: "))
+
+if numOne > numTwo :
+    print("Your number is greater than %d" % numTwo)
+else:
+    print("Your number is less than or equal to %d" % numTwo)
