55727f4698518aff09d9f452139ab62d6ee3915a
nmanhas2 <nubal.manhas1@gmail.com>
2025-01-19 15:05:26 -0700

    Completed Lab2 question 3

---
+++ Lab2/Q3.py
@@ -0,0 +1,30 @@
+#write an if-else statement with multiple
+#branches. If year is 2101 or later, print
+#Distant future. Otherwise, if the year is
+#2001 or greater, print 21st century. Otherwise,
+#if the year is 1901 or greater, print 20th century
+#Else, 1900 or earlier, print Long ago
+
+#if year >= 2101 print Distant future
+#elif year >= 2001 print 21st century
+#elif year >= 1901, print 20th century
+#else print long ago
+
+year = input("Enter the year: ")
+
+if year.isdigit():
+    year = int(year)
+    if year >= 0:
+        if year >= 2101:
+            print("Distant future")
+        elif year >= 2001:
+            print("21st century")
+        elif year >= 1901:
+            print("20th century")
+        else:
+            print("Long ago")
+    else:
+        print("Please enter a positive number.")
+else:
+    print("Please enter a positive whole number.")
+    
