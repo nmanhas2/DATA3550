ed0cea039ec6b408e328acac5ba4b7188ae1029b
nmanhas2 <nubal.manhas1@gmail.com>
2025-01-19 15:29:17 -0700

    Completed Lab2 Question 5

--- Lab2/Q5.py
+++ Lab2/Q5.py
@@ -6,5 +6,35 @@
 
 #if input is a digit
 #   if other input is F or f
-#       if input 
+#       if input > 212F print gaseous
+#       elif input < 32 print solid
+#       else print liquid
 #   elif input is C or c
+#       if input > 100 print gaseous
+#       elif input < 0 print solid
+#       else print liquid
+#else error
+
+temperature = input("Enter the temperature: ")
+unit = input("Enter the unit of temperature (C/F): ")
+
+if temperature.isdigit():
+    temperature = int(temperature)
+    if unit == "F" or unit == "f":
+        if temperature >= 212:
+            print("At that temperature, the water is gaseous.")
+        elif temperature < 32:
+            print("At that temperature, the water is solid.")
+        else:
+            print("At that temperature, the water is liquid")
+    elif unit == "C" or unit == "c":
+        if temperature >= 100:
+            print("At that temperature, the water is gaseous.")
+        elif temperature < 0:
+            print("At that temperature, the water is solid.")
+        else:
+            print("At that temperature, the water is liquid")
+    else:
+        print("Please enter a valid unit of temperature.")
+else:
+    print("Please enter an integer.")
