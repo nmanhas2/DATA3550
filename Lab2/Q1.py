23d0b982eb5f0dfb3df15319e77cc4b9b18c7aa3
nmanhas2 <nubal.manhas1@gmail.com>
2025-01-19 14:48:54 -0700

    Completed Lab2 question 1

---
+++ Lab2/Q1.py
@@ -0,0 +1,10 @@
+#Write an expression that will cause the
+#following code to print "30 or less" if
+#the value of user input is 30 or less
+
+userNumber = float(input("Enter a number: "))
+
+if userNumber <= 30:
+    print("30 or less")
+else:
+    print("Over 30")
