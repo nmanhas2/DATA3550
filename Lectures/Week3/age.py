76cf48e448fd53b44be6c30efa08a8a51ed7797f
nmanhas2 <manhasnubal@gmail.com>
2025-01-18 17:24:44 -0700

    added raining conditional statement problem

---
+++ Lectures/Week3/age.py
@@ -0,0 +1,7 @@
+# A music festival in Vancouver turned into a 
+# riot that's believed to have caused hundreds
+# of thousands of dollars in property damage 
+# late Sunday after organizers announced the 
+# night's headliner wouldn't be performing 
+# because of an illness.
+
---
+++ Lectures/Week3/rain.py
@@ -0,0 +1,14 @@
+#If it rains tomorrow, I will 
+# clean up the basement. Then I will 
+# tidy the cupboards and sort the photos. 
+# Otherwise, I will go swimming. In the 
+# evening I will go to the cinema with my wife.
+
+raining = input("Is it raining?(y/n): ")
+
+if raining == "y":
+    print("I will clean up the basement, tidy cupboards, and sort photos.")
+else:
+    print("I will go swimming")
+
+print("Afterwards, I will go to the cinema.")
