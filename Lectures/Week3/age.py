f7d67a8014231f5f696ae015d5dd70c553a24c58
nmanhas2 <manhasnubal@gmail.com>
2025-01-18 17:31:37 -0700

    added age problem

--- Lectures/Week3/age.py
+++ Lectures/Week3/age.py
@@ -5,3 +5,11 @@
 # night's headliner wouldn't be performing 
 # because of an illness.
 
+age = input("How old are you? ")
+
+if int(age) >= 18:
+    print("You can buy the ticket to Breakout Fest.")
+else:
+    print("You can't buy the ticket to Breakout Fest.")
+
+print("Move on.")
