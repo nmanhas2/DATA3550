38258d8bdb1c55e17e4938c21e4680f72a8481d2
nmanhas2 <nubal.manhas1@gmail.com>
2025-02-17 12:34:30 -0700

    Completed testing list functions

---
+++ Lectures/Week7/listfunctions.py
@@ -0,0 +1,14 @@
+#you can use sum()
+list1 = [0, 1, 2, 3, 4]
+print(sum(list1))
+
+#you can find the min()
+list2 = [2, 34, 76, 57.3]
+print(min(list2))
+
+#you can find the max()
+print(max(list2))
+
+#you can sort
+list2.sort()
+print(list2)
