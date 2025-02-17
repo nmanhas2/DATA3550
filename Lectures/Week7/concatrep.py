43b6cd18e2f426068493d2ad1c67b3e1213d4bba
nmanhas2 <nubal.manhas1@gmail.com>
2025-02-17 12:28:08 -0700

    Completed concatenation and replication problems

---
+++ Lectures/Week7/concatrep.py
@@ -0,0 +1,12 @@
+#concatenating using the + operator
+myFriends = ["Fritz", "Cindy"]
+yourFriends = ["Lee", "Pat", "Phuong"]
+
+ourFriends = myFriends + yourFriends
+
+print(ourFriends)
+
+#replicating with the * operator
+monthInQuarter = [1, 2, 3] * 4   # The list is [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3]
+
+print(monthInQuarter)
